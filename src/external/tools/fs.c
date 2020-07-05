ed49df83049b2fb4e9bf0e8fb06867c30dbdaf1c
Yul3n <yul3n.falx@protonmail.com>
2020-07-05 19:47:41 +0200

    Continued tools.

--- src/external/tools/fs.c
+++ src/external/tools/fs.c
@@ -2,12 +2,13 @@
 #include "fs.h"
 
 char *
-long_to_chars(long l, char *c)
+long_to_chars(long l, char *c, int n)
 {
         long j;
 
         j = 256;
-        for (int i = 0; i < 8; ++i) {
+        --c;
+        for (int i = 0; i < n; ++i) {
                 *(++c) = l & j;
                 j <<= 3;
         }
@@ -20,6 +21,7 @@ create_file(file f)
 {
         char *c;
         long pos;
+        int prev;
 
         c = calloc((((f.size + 511) >> 9) + 2) << 9, sizeof(char));
         if (!c) {
@@ -27,25 +29,31 @@ create_file(file f)
                 exit(1);
         }
         strncpy(c, f.name, 100);
-        long_to_chars(f.uid, c + 108);
-        long_to_chars(f.gid, c + 116);
-        long_to_chars(f.size, c + 128);
+        long_to_chars(f.uid, c + 108, 8);
+        long_to_chars(f.gid, c + 116, 8);
+        long_to_chars(f.size, c + 128, 8);
         strncpy(c + 261, f.uname, 32);
         strncpy(c + 293, f.gname, 32);
-        strncpy(c + 341, f.gname, 167);
+        strncpy(c + 341, f.path, 167);
         pos = 512;
-        while (f.size > 0) {
-                fgets(c + pos + 4, 508, f.file);
-                long_to_chars(pos - 512, c + pos);
+        prev = 508;
+        while (f.size >= 508) {
+                long_to_chars(pos, c + prev, 4);
+                fread(c + pos + 4, 508, 1, f.file);
+                prev = pos;
                 pos += 512;
                 f.size -= 508;
-        } return c;
+        } long_to_chars(pos, c + prev, 4);
+        fread(c + pos + 4, f.size, 1, f.file);
+        return c;
 }
 
 int
 main(int argc, char **argv)
 {
         file f;
+        char *c;
+        FILE *out;
 
         if (argc < 2) {
                 puts("Error: The program has to be called with an argument.");
@@ -61,4 +69,11 @@ main(int argc, char **argv)
         f.path = "/";
         f.gname = "";
         f.uname = "root";
+        c = create_file(f);
+        fclose(f.file);
+        out = fopen("root.fs", "w");
+        fwrite(c, (((f.size + 511) >> 9) + 2) << 9, 1, out);
+        fclose(out);
+        free(c);
+        return 0;
 }
