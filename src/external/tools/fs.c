3ee370c142248724d4621fbb5d2f33b0ab6a8e47
Yul3n <aghilas.boussaa@laposte.net>
2020-07-05 11:22:00 +0200

    Continued the disk creator.

--- src/external/tools/fs.c
+++ src/external/tools/fs.c
@@ -3,12 +3,10 @@
 #include "fs.h"
 
 char *
-long_to_chars(long l)
+long_to_chars(long l, char *c)
 {
-        char *c;
         long j;
 
-        c = malloc(8);
         j = 256;
         for (int i = 0; i < 8; ++i) {
                 *(++c) = l & j;
@@ -22,18 +20,21 @@ char *
 create_file(file f)
 {
         char *c;
-        char *buf;
+        char *prev;
+        int pos;
 
         c = calloc((((f.size + 511) >> 9) + 2) << 9, sizeof(char));
         strncpy(c, f.name, 100);
-        buf = long_to_chars(f.uid);
-        memcpy(c + 108, buf, 8);
-        free(buf);
-        buf = long_to_chars(f.gid);
-        memcpy(c + 116, buf, 8);
-        free(buf);
-        buf = long_to_chars(f.size);
-        memcpy(c + 128, buf, 8);
-        free(buf);
+        long_to_chars(f.uid, c + 108);
+        long_to_chars(f.gid, c + 116);
+        long_to_chars(f.size, c + 128);
+        strncpy(c + 261, f.uname, 32);
+        strncpy(c + 293, f.gname, 32);
+        strncpy(c + 341, f.gname, 167);
+        prev = c + 508;
+        while (f.size <= 508) {
+                pos += 512;
+                f.size -= 508;
+        }
         return c;
 }
